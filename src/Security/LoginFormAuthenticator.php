ef7bb38443d9c8ef528caf8e5d95d27021028d1c
KaeligShebam <kaelig@shebam.fr>
2021-07-13 11:43:53 +0200

    Add function

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -97,7 +97,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-       return new RedirectResponse($this->urlGenerator->generate('account'));
+       return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
