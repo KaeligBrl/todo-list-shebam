be7a04ca332ad0654e84abc634674fdca06cb3e1
KaeligShebam <kaelig@shebam.fr>
2024-05-17 09:13:22 +0200

    Corrige taskdone

--- public/assets/js/checkbox.js
+++ public/assets/js/checkbox.js
@@ -1,43 +1,25 @@
-window.onload = () => {
-    let taskdone = document.querySelectorAll(".taskdone");
+$(document).ready(function () {
+    let taskdoneCheckboxes = $(".taskdone");
+    taskdoneCheckboxes.each(function () {
+        $(this).on("click", function () {
 
-    for (button of taskdone) {
-        button.addEventListener("click", function () {
-            // Récupérez l'identifiant de la tâche
-            let taskId = this.dataset.taskdone;
+            let taskId = $(this).data("taskdone");
+            let trElement = $(`tr[data-taskdone="${taskId}"]`);
 
-            // Récupérez le <tr> correspondant à la tâche
-            let trElement = document.querySelector(`tr[data-taskdone="${taskId}"]`);
-
-            // Ajoutez ou supprimez la classe en fonction de la propriété "checked"
-            if (this.checked) {
-                trElement.classList.add('bg-yellow-dark');
+            if ($(this).is(":checked")) {
+                trElement.addClass('bg-yellow-dark');
             } else {
-                trElement.classList.remove('bg-yellow-dark');
+                trElement.removeClass('bg-yellow-dark');
             }
 
-            // Envoie de la requête AJAX pour mettre à jour la couleur dans le backend
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", `/semaine-actuelle/api/update-task/${taskId}`, true);
-            xhr.setRequestHeader("Content-Type", "application/json");
-
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        // Succès
-                        var response = JSON.parse(xhr.responseText);
-                        console.log('Response from server:', response);
-                    } else {
-                        // Gestion des erreurs
-                        console.error('Erreur lors de la mise à jour de la couleur:', xhr.status, xhr.statusText);
-                    }
-                }
-            };
-
-            // Envoie de la valeur de la couleur au backend
-            xhr.send(JSON.stringify({
-                done: this.checked,
-            }));
+            $.ajax({
+                url: `/semaine-actuelle/api/update-task/${taskId}`,
+                method: "POST",
+                contentType: "application/json",
+                data: JSON.stringify({
+                    done: $(this).is(":checked"),
+                }),
+            });
         });
-    }
-};
+    });
+});
